a55cb134aacf02e0cbe0a23130f2d32b44ca0251
Ingolf Wagner <palo@pepe>
2018-10-25 14:00:12 +0200

    add database tests that fail

--- app/services/database/InMemoryDatabaseService.scala
+++ app/services/database/InMemoryDatabaseService.scala
@@ -1,7 +1,24 @@
 package services.database
 
+import java.util.UUID
+
+import scala.concurrent.Future
+
 class InMemoryDatabaseService {
+
+  var map: Map[UUID, Listing] = Map[UUID, Listing]()
+
+  def find(id: UUID): Future[Option[Listing]] = Future.successful(
+    Some(Listing(Contact(""), Address("", "", "", "", ""), Location(0, 0)))
+  )
+
+  def save(listing: Listing): Future[UUID] = Future.successful(UUID.randomUUID())
 
   def ready() = true
 
 }
+
+case class Listing(contact: Contact, address: Address, location: Location)
+case class Contact(phoneNumber: String)
+case class Address(street: String, postalCode: String, countryCode: String, city: String, stateCode: String)
+case class Location(latitude: Double, longitude: Double)
