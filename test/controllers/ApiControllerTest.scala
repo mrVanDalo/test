af05132b39bb0ee83b8de3df9524eddb50673940
Ingolf Wagner <palo@pepe>
2018-10-26 11:46:26 +0200

    api implimentation done

--- test/controllers/ApiControllerTest.scala
+++ test/controllers/ApiControllerTest.scala
@@ -2,10 +2,11 @@ package controllers
 
 import java.util.UUID
 
+import models.{ Location, OutputAddress, OutputContact, OutputListing }
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.json.Json
+import play.api.libs.json.{ JsValue, Json }
 import play.api.test.Helpers._
 import play.api.test._
 
@@ -57,10 +58,43 @@ class ApiControllerTest extends PlaySpec with GuiceOneAppPerTest with Injecting 
       val injector = new GuiceApplicationBuilder().injector()
       val controller = injector.instanceOf[ApiController]
 
-      val response = controller.post().apply(FakeRequest(POST, "/api/v1/listing/").withJsonBody(Json.parse("{}")))
+      val postResponse = controller.post().apply(FakeRequest(POST, "/api/v1/listing/").withJsonBody(Json.parse(
+        """
+          |{"contact":{
+          |  "phoneNumber":"+41123456"
+          | },
+          | "location":{
+          |  "lng":1,
+          |  "lat":2
+          | },
+          | "address":{
+          |  "street":"waterloo",
+          |  "postalCode":"12321",
+          |  "countryCode":"GB",
+          |  "city":"London",
+          |  "stateCode":"LO"
+          | }
+          |}
+        """.stripMargin)))
+
+      status(postResponse) mustBe OK
+
+      // provoke exception to test if the response is an UUID
+      val result = Json.parse(contentAsString(postResponse))
+      val idValue: JsValue = (result \ "id").get
+      val id = idValue.validate[String].get
+      val uuid = UUID.fromString(id)
+
+      val response = controller.get(id).apply(FakeRequest(GET, s"/api/v1/listing/${id}"))
 
       status(response) mustBe OK
-      Json.parse(contentAsString(response))
+      val listing = Json.parse(contentAsString(response)).validate[OutputListing].get
+
+      listing.id mustBe uuid
+      listing.contact mustBe OutputContact("+41123456", "+41-123-456")
+      listing.address mustBe OutputAddress(street = "waterloo", postalCode = "12321", countryCode = "GB", city = "London", stateCode = "LO", country = "United Kingdom of Great Britain and Northern Ireland")
+      listing.location mustBe Location(lng = 1, lat = 2)
+
     }
 
   }
